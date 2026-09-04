c40aec87db207b1eff23b10560a57b73c65346dd
newt_00 <kaiohara@icloud.com>
2026-09-04 09:19:12 -0400

    upgraded app version to 0.8.4

--- src/AppConfig.h
+++ src/AppConfig.h
@@ -3,7 +3,7 @@
 namespace AppConfig
 {
     inline constexpr wchar_t APP_NAME[] = L"NewtTech Launcher";
-    inline constexpr wchar_t APP_VERSION[] = L"0.8.0-beta";
+    inline constexpr wchar_t APP_VERSION[] = L"0.8.4-beta";
 
     inline constexpr wchar_t INDEX_URL[] =
         L"https://launcher.newttech.net/api/index.php";
